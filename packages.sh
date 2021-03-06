a2d54752736d549a06820975ed75101172ed8b32
7h1b0 <7h1b0@users.noreply.github.com>
2021-03-06 17:28:44 +0100

    Remove Insomnia

--- packages.sh
+++ packages.sh
@@ -19,7 +19,6 @@ install_packages() {
       "nodejs"
       "docker.io"
       "docker-compose"
-      "insomnia"
       "abiword"
       "gnumeric"
       "gimp"
@@ -68,11 +67,6 @@ source_packages() {
     printf 'Adding vscodium source...\n'
     wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg
     echo 'deb [signed-by=/etc/apt/trusted.gpg.d/vscodium-archive-keyring.gpg] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
-
-
-    printf 'Adding Insomnia source...\n'
-    wget -qO - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
-    echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee /etc/apt/sources.list.d/insomnia.list
 
     printf 'Adding Spotify source...\n'
     wget -qO - https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add
